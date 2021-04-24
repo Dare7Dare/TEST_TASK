0447e83bf6d72f89309b636f1bc53cc28caa6608
Dare7Dare <72170957+Dare7Dare@users.noreply.github.com>
2021-04-24 22:54:03 +0200

    pricing section

--- js/main.js
+++ js/main.js
@@ -28,3 +28,54 @@ function toggleMenu() {
         showMenu = false;
     }
 }
+
+let detailsBusinessShow = false;
+let detailsBasicShow = false;
+let detailsPremiumShow = false;
+const showBusiness = document.querySelector('.show-business');
+showBusiness.addEventListener('click', showBusinessDetails);
+const showPremium = document.querySelector('.show-premium');
+showPremium.addEventListener('click', showPremiumDetails);
+const showBasic = document.querySelector('.show-basic');
+showBasic.addEventListener('click', showBasicDetails);
+
+const detailsBusiness = document.querySelector('.business-details');
+const detailsPremium = document.querySelector('.premium-details');
+const detailsBasic = document.querySelector('.basic-details');
+
+function showBusinessDetails() {
+    if(!detailsBusinessShow) {
+        showBusiness.classList.add('show');
+        detailsBusiness.classList.add('visible');
+        detailsBusinessShow = true;
+    }
+    else {
+        showBusiness.classList.remove('show');
+        detailsBusiness.classList.remove('visible');
+        detailsBusinessShow = false;
+    }
+}
+function showPremiumDetails() {
+    if(!detailsPremiumShow) {
+        showPremium.classList.add('show');
+        detailsPremium.classList.add('visible');
+        detailsPremiumShow = true;
+    }
+    else {
+        showPremium.classList.remove('show');
+        detailsPremium.classList.remove('visible');
+        detailsPremiumShow = false;
+    }
+}
+function showBasicDetails() {
+    if(!detailsBasicShow) {
+        showBasic.classList.add('show');
+        detailsBasic.classList.add('visible');
+        detailsBasicShow = true;
+    }
+    else {
+        showBasic.classList.remove('show');
+        detailsBasic.classList.remove('visible');
+        detailsBasicShow = false;
+    }
+}
